0a4f51fe7305e28ba8387caee0daf5d0cf5e3032
Gianni Angelozzi <gianni@sistema3.it>
2022-08-14 00:10:18 +0200

    add data annotations

--- TNBU.GUI/EF/Models/WiFiNetwork.cs
+++ TNBU.GUI/EF/Models/WiFiNetwork.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TNBU.GUI.EF.Models {
 	public class WiFiNetwork {
 		public long ID { get; set; }
+		[Required]
 		public string SSID { get; set; }
+		[Required, MinLength(8)]
 		public string Password { get; set; }
 	}
 }

0728f4507734f0b43c1e178ed4f2f79d8326773d
Gianni Angelozzi <gianni@sistema3.it>
2022-08-15 00:45:07 +0200

    PoeEnabled

--- TNBU.Core/Models/DeviceConfiguration/CfgSwitchEntry.cs
+++ TNBU.Core/Models/DeviceConfiguration/CfgSwitchEntry.cs
@@ -1,5 +1,6 @@
 namespace TNBU.Core.Models.DeviceConfiguration {
 	public class CfgSwitchEntry {
+		public bool PoeEnabled { get; set; } = true;
 		public string GetConfig(int num) {
 			return $@"
 switch.port.{num}.keepalive=disabled
@@ -7,7 +8,7 @@ switch.port.{num}.lldpmed.opmode=enabled
 switch.port.{num}.lldpmed.topology_notify=disabled
 switch.port.{num}.name=Port {num}
 switch.port.{num}.opmode=switch
-switch.port.{num}.poe=auto
+switch.port.{num}.poe={(PoeEnabled?"auto":"shutdown")}
 switch.port.{num}.port-security=disabled
 switch.port.{num}.stp.port_mode=enabled
 ";

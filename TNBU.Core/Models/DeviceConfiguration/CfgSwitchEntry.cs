0711b293536825a0175de296f099e54631ee6bcb
Gianni Angelozzi <gianni@sistema3.it>
2022-08-16 11:55:46 +0200

    lag

--- TNBU.Core/Models/DeviceConfiguration/CfgSwitchEntry.cs
+++ TNBU.Core/Models/DeviceConfiguration/CfgSwitchEntry.cs
@@ -1,17 +1,34 @@
 namespace TNBU.Core.Models.DeviceConfiguration {
 	public class CfgSwitchEntry {
+		public bool Enabled { get; set; } = true;
 		public bool PoeEnabled { get; set; } = true;
+		public int? LAG { get; set; }
 		public string GetConfig(int num) {
-			return $@"
+			var OpMode = "switch";
+			if(LAG.HasValue) {
+				OpMode = "aggregate";
+			}
+			var ret = $@"
 switch.port.{num}.keepalive=disabled
 switch.port.{num}.lldpmed.opmode=enabled
 switch.port.{num}.lldpmed.topology_notify=disabled
 switch.port.{num}.name=Port {num}
-switch.port.{num}.opmode=switch
-switch.port.{num}.poe={(PoeEnabled?"auto":"shutdown")}
+switch.port.{num}.opmode={OpMode}
+switch.port.{num}.poe={(PoeEnabled ? "auto" : "shutdown")}
 switch.port.{num}.port-security=disabled
 switch.port.{num}.stp.port_mode=enabled
 ";
+			if(!Enabled) {
+				ret += $@"
+switch.port.{num}.status=disabled
+";
+			}
+			if(LAG.HasValue) {
+				ret += $@"
+switch.port.{num}.lag={LAG.Value}
+";
+			}
+			return ret;
 		}
 	}
 }

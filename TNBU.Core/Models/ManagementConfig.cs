fd607ed3d2722a57c2e0ffb11e11f9ef8d04d2e4
Gianni Angelozzi <gianni@sistema3.it>
2022-08-26 11:14:00 +0200

    add ManagementConfig fields

--- TNBU.Core/Models/ManagementConfig.cs
+++ TNBU.Core/Models/ManagementConfig.cs
@@ -2,20 +2,41 @@ namespace TNBU.Core.Models {
 	public class ManagementConfig {
 		public string CfgVersion { get; set; } = "?";
 		public bool LedEnabled { get; set; } = true;
+		public string? MgmtUrl { get; set; }
+		public string? StunUrl { get; set; }
+		public string? InformUrl { get; set; }
+		public string? AuthKey { get; set; }
 
 		public override string ToString() {
-			/*
-				mgmt_url=
-				stun_url=
-			*/
-			return $@"
+			var ret = $@"
 capability=notif,fastapply-bg,notif-assoc-stat
 cfgversion={CfgVersion}
 led_enabled={(LedEnabled ? "true" : "false")}
 report_crash=true
 selfrun_guest_mode=pass
 use_aes_gcm=true
-".Replace(Environment.NewLine, "\n");
+";
+			if(MgmtUrl != null) {
+				ret += $@"
+mgmt_url={MgmtUrl}
+";
+			}
+			if(StunUrl != null) {
+				ret += $@"
+stun_url={StunUrl}
+";
+			}
+			if(AuthKey != null) {
+				ret += $@"
+authkey={AuthKey}
+";
+			}
+			if(InformUrl != null) {
+				ret += $@"
+inform_url={InformUrl}
+";
+			}
+			return ret.Replace(Environment.NewLine, "\n");
 		}
 	}
 }

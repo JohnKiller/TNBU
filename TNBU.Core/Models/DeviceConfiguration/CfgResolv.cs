cac96f97cc5e95d2cd1d1b9212193bf6584dafcf
Gianni Angelozzi <gianni@sistema3.it>
2023-04-16 00:18:20 +0200

    hostname support

--- TNBU.Core/Models/DeviceConfiguration/CfgResolv.cs
+++ TNBU.Core/Models/DeviceConfiguration/CfgResolv.cs
@@ -1,11 +1,18 @@
 namespace TNBU.Core.Models.DeviceConfiguration {
 	public class CfgResolv {
+		public string? HostName { get; set; }
 		public override string ToString() {
-			return $@"
+			var ret = $@"
 resolv.status=enabled
 resolv.nameserver.1.status=disabled
 resolv.nameserver.2.status=disabled
 ";
+			if(HostName != null) {
+				ret += $@"
+resolv.host.1.name={HostName}
+";
+			}
+			return ret;
 		}
 	}
 }
